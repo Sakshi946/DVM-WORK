1d5cdf92ffe968cf24e4630bf984b8f84f635202
Sakshi946 <sakshipriya2204@gmail.com>
2026-04-07 18:39:29 +0530

    Update Outliers_MissingData.py

--- Outliers_MissingData.py
+++ Outliers_MissingData.py
@@ -1,18 +1,14 @@
 import pandas as pd
 import numpy as np
 
-# Dataset with missing values & outlier
 data = [10, 20, 30, None, 40, 1000]
 
 df = pd.DataFrame(data, columns=["Values"])
 
-# Check missing values
 print("Missing values:\n", df.isnull())
 
-# Fill missing with mean
 df["Values"].fillna(df["Values"].mean(), inplace=True)
 
-# Detect outliers using IQR
 Q1 = df["Values"].quantile(0.25)
 Q3 = df["Values"].quantile(0.75)
 IQR = Q3 - Q1
